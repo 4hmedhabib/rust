1087f6a2f2efd70fa4662254843cc967df1f49f6
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-02-17 06:43:38 +0300

    added first helloworld print

---
+++ first-app/target/debug/deps/first_app.d
@@ -0,0 +1,5 @@
+E:\Software Engineering\Web Development\Path\Rust\first-app\target\debug\deps\first_app.exe: src\main.rs
+
+E:\Software Engineering\Web Development\Path\Rust\first-app\target\debug\deps\first_app.d: src\main.rs
+
+src\main.rs:
